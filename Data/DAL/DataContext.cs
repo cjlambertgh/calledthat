afc7b776f3a24ef1116319d669c381649d877b53
chris <phateuk@gmail.com>
2017-03-09 13:11:19 +0000

    add identity models and migration

--- Data/DAL/DataContext.cs
+++ Data/DAL/DataContext.cs
@@ -17,6 +17,9 @@ namespace Data
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<IdentityUserLogin>().HasKey<string>(l => l.UserId);
+            modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id);
+            modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
         }
 
         DbSet<Team> Teams { get; set; }
---
+++ Data/Migrations/201703091309188_add-identity.cs
@@ -0,0 +1,100 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addidentity : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.IdentityRole",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserRole",
+                c => new
+                    {
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        IdentityRole_Id = c.String(maxLength: 128),
+                        AppUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.RoleId, t.UserId })
+                .ForeignKey("dbo.IdentityRole", t => t.IdentityRole_Id)
+                .ForeignKey("dbo.AppUser", t => t.AppUser_Id)
+                .Index(t => t.IdentityRole_Id)
+                .Index(t => t.AppUser_Id);
+            
+            CreateTable(
+                "dbo.AppUser",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserClaim",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        AppUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AppUser", t => t.AppUser_Id)
+                .Index(t => t.AppUser_Id);
+            
+            CreateTable(
+                "dbo.IdentityUserLogin",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(),
+                        ProviderKey = c.String(),
+                        AppUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.AppUser", t => t.AppUser_Id)
+                .Index(t => t.AppUser_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.IdentityUserRole", "AppUser_Id", "dbo.AppUser");
+            DropForeignKey("dbo.IdentityUserLogin", "AppUser_Id", "dbo.AppUser");
+            DropForeignKey("dbo.IdentityUserClaim", "AppUser_Id", "dbo.AppUser");
+            DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropIndex("dbo.IdentityUserLogin", new[] { "AppUser_Id" });
+            DropIndex("dbo.IdentityUserClaim", new[] { "AppUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "AppUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropTable("dbo.IdentityUserLogin");
+            DropTable("dbo.IdentityUserClaim");
+            DropTable("dbo.AppUser");
+            DropTable("dbo.IdentityUserRole");
+            DropTable("dbo.IdentityRole");
+        }
+    }
+}

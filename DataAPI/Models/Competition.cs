f3ec15b0e719698c704aba9706cd8fcf57b46024
chris <phateuk@gmail.com>
2017-03-29 22:05:41 +0100

    Add matchday details to competition

--- DataAPI/Models/Competition.cs
+++ DataAPI/Models/Competition.cs
@@ -12,6 +12,8 @@ namespace DataAPI.Models
         public string Caption { get; set; }
         public string League { get; set; }
         public string Year { get; set; }
+        public int CurrentMatchDay { get; set; }
+        public int NumberOfMatchDays { get; set; }
         public int NumberOfTeams { get; set; }
         public int NumberOfGames { get; set; }
         public string LastUpdated { get; set; }

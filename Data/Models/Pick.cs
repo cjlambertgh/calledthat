e0d04e66558e22b6b35bbbfe8cc755d9f459a0d5
chris <phateuk@gmail.com>
2017-06-04 15:28:05 +0100

    Add models for Pick and PickResult

---
+++ Data/Models/Pick.cs
@@ -0,0 +1,33 @@
+﻿using Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Pick : IModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid PlayerId { get; set; }
+
+        [ForeignKey("PlayerId")]
+        public Player Player { get; set; }
+
+        public Guid FixtureId { get; set; }
+
+        [ForeignKey("FixtureID")]
+        public Fixture Fixture { get; set; }
+
+        public int HomeScore { get; set; }
+
+        public int AwayScore { get; set; }
+
+        public bool Banker { get; set; }
+
+        public bool Double { get; set; }
+    }
+}
---
+++ Data/Models/PickResult.cs
@@ -0,0 +1,23 @@
+﻿using Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class PickResult: IModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid PickId { get; set; }
+
+        [ForeignKey("PickId")]
+        public Pick Pick { get; set; }
+
+        public int Points { get; set; }
+
+    }
+}

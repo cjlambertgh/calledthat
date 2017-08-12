4de66291a24c97edcb9e1b11ac5620c3ac8339c8
chris <phateuk@gmail.com>
2017-08-12 11:00:26 +0100

    Add picks form submission added

--- CalledThat/Controllers/GameController.cs
+++ CalledThat/Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using CalledThat.ViewModels;
+﻿using AppServices;
+using CalledThat.ViewModels;
 using GameService;
 using System;
 using System.Collections.Generic;
@@ -8,16 +9,18 @@ using System.Web.Mvc;
 
 namespace CalledThat.Controllers
 {
-    public class GameController : Controller
+    public class GameController : BaseController
     {
         private readonly IGameService _gameService;
 
-        public GameController(IGameService gameService)
+        public GameController(IGameService gameService, IUserService userService)
+            :base(userService)
         {
             _gameService = gameService;
         }
 
         // GET: Game
+        [HttpGet]
         [Authorize]
         public ActionResult Index()
         {
@@ -42,6 +45,31 @@ namespace CalledThat.Controllers
             }
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(AddPicksViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var playerId = CurrentUser.Players.FirstOrDefault()?.Id;
+
+            if(playerId == null)
+            {
+                throw new ArgumentNullException(nameof(playerId));
+            }
+
+            model.PickItems.ForEach(item =>
+            {
+                _gameService.AddPick((Guid)playerId, item.FixtureId, int.Parse(item.HomeScore), int.Parse(item.AwayScore), item.Banker, item.Double);
+            });
+
+            return View(model);
         }
     }
 }

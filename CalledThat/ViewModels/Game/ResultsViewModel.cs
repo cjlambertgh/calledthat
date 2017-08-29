ee984e31fc7123b739defcf9484b398e5b2e498b
chris <phateuk@gmail.com>
2017-08-29 19:10:00 +0100

    add result viewmodel

---
+++ CalledThat/ViewModels/Game/ResultsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalledThat.ViewModels.Game
+{
+    public class ResultsViewModel
+    {
+        public Dictionary<int, ResultItem> ResultItems { get; set; }
+        public int Gameweek { get; set; }
+        public int GameweekPoints { get; set; }
+
+        public ResultsViewModel()
+        {
+            ResultItems = new Dictionary<int, ResultItem>();
+        }
+
+        public class ResultItem
+        {
+            public string HomeTeam { get; set; }
+            public string AwayTeam { get; set; }
+            public int HomeScore { get; set; }
+            public int AwayScore { get; set; }
+            public int HomePick { get; set; }
+            public int AwayPick { get; set; }
+            public int Points { get; set; }
+        }
+    }
+}

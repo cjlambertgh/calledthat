625b2d37966e36816f23d4bcd11ba8d0591c33ec
chris <phateuk@gmail.com>
2017-09-19 18:49:57 +0100

    Add EmailService

---
+++ EmailService/HttpMailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    public class HttpMailService : IMailService
+    {
+        public void Send(string subject, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

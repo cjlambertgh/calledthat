b21e2df30f558019e511fcf88536ca2e3acf603c
chris <phateuk@gmail.com>
2018-01-31 18:43:50 +0000

    Include raw sql for updated proc

--- Data/SQL/uspGetLeagueStats.sql
+++ Data/SQL/uspGetLeagueStats.sql
@@ -20,74 +20,168 @@ GO
 CREATE procedure [dbo].[uspGetLeagueStats] 
 @leagueId varchar(255) 
 as
-DECLARE @stats TABLE (
-  Playerid uniqueidentifier,
-  PlayerName nvarchar(256),
-  GameweekNumber int,
-  Points int,
-  Stat nvarchar(64)
-)
+DECLARE @stats TABLE 
+  ( 
+     playerid       UNIQUEIDENTIFIER, 
+     playername     NVARCHAR(256), 
+     gameweeknumber INT, 
+     points         INT, 
+     stat           NVARCHAR(64) 
+  ) 
+DECLARE @gameweekPointsData TABLE 
+  ( 
+     playerid       UNIQUEIDENTIFIER, 
+     playername     NVARCHAR(256), 
+     gameweeknumber INT, 
+     gameweekpoints INT 
+  ) 
+DECLARE @gameweekFixturePoints TABLE 
+  ( 
+     playerid       UNIQUEIDENTIFIER, 
+     playername     NVARCHAR(256), 
+     gameweeknumber INT, 
+     gameweekpoints INT 
+  ) 
 
-DECLARE @data TABLE (
-  playerId uniqueidentifier,
-  PlayerName nvarchar(256),
-  GameweekNumber int,
-  GameweekPoints int
-)
+INSERT INTO @gameweekPointsData 
+SELECT p.id           playerId, 
+       p.NAME         AS PlayerName, 
+       g.number       AS GameweekNumber, 
+       Sum(pr.points) GameweekPoints 
+FROM   player p 
+       JOIN playerleagues pl 
+         ON pl.playerid = p.id 
+       JOIN league l 
+         ON pl.leagueid = l.id 
+       LEFT JOIN pick pic 
+              ON pic.playerid = p.id 
+       LEFT JOIN fixture f 
+              ON pic.fixtureid = f.id 
+       LEFT JOIN gameweek g 
+              ON f.gameweekid = g.id 
+       LEFT JOIN pickresult pr 
+              ON pic.id = pr.pickid 
+       LEFT JOIN competition c 
+              ON l.competitionid = c.id 
+WHERE  l.id = @leagueId 
+GROUP  BY p.id, 
+          p.NAME, 
+          g.number 
 
-INSERT INTO @data
-  SELECT
-    p.Id playerId,
-	p.Name AS PlayerName,
-    g.Number AS GameweekNumber,
-    SUM(pr.Points) GameweekPoints
-  FROM player p
-  JOIN PlayerLeagues pl
-    ON pl.PlayerId = p.Id
-  JOIN League l
-    ON pl.LeagueId = l.Id
-  LEFT JOIN Pick pic
-    ON pic.PlayerId = p.Id
-  LEFT JOIN Fixture f
-    ON pic.FixtureId = f.Id
-  LEFT JOIN GameWeek g
-    ON f.GameWeekId = g.Id
-  LEFT JOIN PickResult pr
-    ON pic.id = pr.PickId
-  LEFT JOIN Competition c
-    ON l.CompetitionId = c.Id
-  WHERE l.id = @leagueId
-  GROUP BY p.id, p.Name,
-           g.Number
+INSERT INTO @gameweekFixturePoints 
+            (playerid, 
+             playername, 
+             gameweeknumber, 
+             gameweekpoints) 
+SELECT player.id   PlayerId, 
+       player.NAME PlayerName, 
+       gw.number   GameweekNumber, 
+       pr.points 
+FROM   fixture f 
+       JOIN result r 
+         ON f.id = r.fixtureid 
+       JOIN pick p 
+         ON f.id = p.fixtureid 
+       JOIN pickresult pr 
+         ON p.id = pr.pickid 
+       JOIN player player 
+         ON player.id = p.playerid 
+       JOIN gameweek gw 
+         ON gw.id = f.gameweekid 
+       JOIN playerleagues pl 
+         ON pl.playerid = player.id 
+       JOIN league l 
+         ON pl.leagueid = l.id 
+WHERE  l.id = @leagueId 
 
+--Highest gameweek score 
+INSERT INTO @stats 
+            (playerid, 
+             playername, 
+             gameweeknumber, 
+             points, 
+             stat) 
+SELECT playerid, 
+       playername, 
+       gameweeknumber, 
+       gameweekpoints, 
+       'HighestScore' 
+FROM   @gameweekPointsData d 
+       JOIN (SELECT Max(gameweekpoints) points 
+             FROM   @gameweekPointsData) AS highest 
+         ON highest.points = d.gameweekpoints 
 
-INSERT INTO @stats (PlayerId, PlayerName, GameweekNumber, Points, Stat)
-  SELECT
-    playerId,
-	PlayerName,
-    GameWeekNumber,
-    GameweekPoints,
-    'HighestScore'
-  FROM @data d
-  JOIN (SELECT
-    MAX(GameweekPoints) points
-  FROM @data) AS highest
-    ON highest.points = d.GameweekPoints
+--Lowest gameweek score 
+INSERT INTO @stats 
+            (playerid, 
+             playername, 
+             gameweeknumber, 
+             points, 
+             stat) 
+SELECT playerid, 
+       playername, 
+       gameweeknumber, 
+       gameweekpoints, 
+       'LowestScore' 
+FROM   @gameweekPointsData d 
+       JOIN (SELECT Min(gameweekpoints) points 
+             FROM   @gameweekPointsData) AS highest 
+         ON highest.points = d.gameweekpoints 
 
-INSERT INTO @stats (PlayerId, PlayerName, GameweekNumber, Points, Stat)
-  SELECT
-    playerId,
-	PlayerName,
-    GameWeekNumber,
-    GameweekPoints,
-    'LowestScore'
-  FROM @data d
-  JOIN (SELECT
-    MIN(GameweekPoints) points
-  FROM @data) AS highest
-    ON highest.points = d.GameweekPoints
+--most correct scores in a week 
+INSERT INTO @stats 
+            (playerid, 
+             playername, 
+             gameweeknumber, 
+             points, 
+             stat) 
+SELECT TOP 1 playerid, 
+             playername, 
+             gameweeknumber, 
+             Count(*)            Points, 
+             'MostCorrectInWeek' Stat 
+FROM   @gameweekFixturePoints d 
+WHERE  d.gameweekpoints >= 10 
+GROUP  BY playerid, 
+          playername, 
+          gameweeknumber 
+ORDER  BY points DESC 
 
-SELECT
-  *
-FROM @Stats
+--Highest scoring gameweek 
+INSERT INTO @stats 
+            (playerid, 
+             playername, 
+             gameweeknumber, 
+             points, 
+             stat) 
+SELECT TOP 1 NULL                     PlayerId, 
+             NULL                     PlayerName, 
+             gameweeknumber, 
+             Sum(gameweekpoints)      Points, 
+             'HighestScoringGameweek' Stat 
+FROM   @gameweekFixturePoints d 
+GROUP  BY gameweeknumber 
+ORDER  BY points DESC 
+
+--Average score per week 
+INSERT INTO @stats 
+            (playerid, 
+             playername, 
+             gameweeknumber, 
+             points, 
+             stat) 
+SELECT NULL                   PlayerId, 
+       NULL                   PlayerName, 
+       NULL                   GameweekNumber, 
+       Avg(gameweekpoints)    Points, 
+       'AverageGameweekScore' Stat 
+FROM   (SELECT gameweeknumber, 
+               Sum(gameweekpoints) GameweekPoints 
+        FROM   @gameweekFixturePoints d 
+        GROUP  BY gameweeknumber) a 
+
+SELECT * 
+FROM   @Stats 
 GO
+
+
